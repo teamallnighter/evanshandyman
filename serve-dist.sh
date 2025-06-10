89449c99ac8212f125dda691ad33a53a82e45288
Chris Connelly <connellychris@me.com>
2025-06-10 00:31:34 -0300

    Fixing

--- serve-dist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting local server for built version..."
-echo "Open http://localhost:8080 in your browser"
-echo "Press Ctrl+C to stop the server"
-cd dist && python3 -m http.server 8080
-
